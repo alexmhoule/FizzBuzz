60ce5a15ffd56300cbe4c09d9dd1a8b5a4bd1c57
alexmhoule <alexmhoule@gmail.com>
2015-05-01 01:55:59 -0500

    initial push

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+	for (var i = 1; i <= 100; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			console.log("FizzBuzz");
+			$('ul').append('<li>' + "FizzBuzz" + '</li>');
+		} else if (i % 3 == 0) {
+			console.log("Fizz");
+			$('ul').append('<li>' + "Fizz" + '</li>');
+		} else if (i % 5 == 0) {
+			console.log("Buzz");
+			$('ul').append('<li>' + "Buzz" + '</li>');
+		} else {
+			console.log (i);
+			$('ul').append('<li>' + i + '</li>');
+		}
+	};	
+});
