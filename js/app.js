3a228ead77c942b09f0f3bc395d1dcca06092ad9
alexmhoule <alexmhoule@gmail.com>
2015-05-06 03:26:58 -0500

    prompt works

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
 $(document).ready(function () {
-	prompt("Please enter a value to count to:");
-	for (var i = 1; i <= 100; i++) {
+	 var myInput = prompt("Please enter a value to count to:"); 
+	 myInputAsInteger = parseInt(myInput);
+
+	$('#submit').click(function () {
+		if( $('input#number').val().trim().length == 0 ) {
+			alert('Whoops, please type a number');
+		} else 
+		 {
+			$('ul').append('<li>' + myInputAsInteger + '</li>');
+			$('input#number').val("");
+		}
+	})
+
+	//var inputted = parseInt($("#number").val());
+
+	for (var i = 1; i <= myInputAsInteger; i++) {
 		if (i % 3 == 0 && i % 5 == 0) {
-			console.log("FizzBuzz");
 			$('ul').append('<li>' + "FizzBuzz" + '</li>');
 		} else if (i % 3 == 0) {
-			console.log("Fizz");
 			$('ul').append('<li>' + "Fizz" + '</li>');
 		} else if (i % 5 == 0) {
-			console.log("Buzz");
 			$('ul').append('<li>' + "Buzz" + '</li>');
 		} else {
-			console.log (i);
 			$('ul').append('<li>' + i + '</li>');
 		}
 	};	
